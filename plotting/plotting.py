d8739d6cb428481efa72dcab2728f55b1399f1a1
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-04-08 14:24:02 +0100

    Fix dynamic plot

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -24,76 +24,44 @@ def main ():
     def animate(i):
         folder_path = "C:\\Users\\nizar\\OneDrive\\Bureau\\output\\"  # Replace with the actual path to your folder
         
-        folder_path_tsla= folder_path+"TSLA"
-
-        files = [f for f in os.listdir(folder_path_tsla) if os.path.isfile(os.path.join(folder_path_tsla, f))]
-        # Sort the files by creation time
-        files_sorted = sorted(files, key=lambda x: os.path.getctime(os.path.join(folder_path_tsla, x)))
-        df = pd.DataFrame()
-        for file in files_sorted:
-            path = folder_path_tsla+"\\"+file
-            df1 = pd.read_csv(path, header=None)
+        file = [f for f in os.listdir(os.path.join(folder_path, "TSLA")) if os.path.isfile(os.path.join(os.path.join(folder_path, "TSLA"), f))][0]
+        df1 = pd.read_csv(os.path.join(folder_path, "TSLA", file), header = None)
         df1[1] = df1[1].apply(lambda s: float(s[:-1]))
 
-
-        
-        folder_path_aapl= folder_path+"AAPL"
-        files = [f for f in os.listdir(folder_path_aapl) if os.path.isfile(os.path.join(folder_path_aapl, f))]
-        # Sort the files by creation time
-        files_sorted = sorted(files, key=lambda x: os.path.getctime(os.path.join(folder_path_aapl, x)))
-        df2 = pd.DataFrame()
-        for file in files_sorted:
-            path = folder_path_aapl+"\\"+file
-            df1 = pd.read_csv(path, header=None)
-            df2 = df2.append(df1)
-
+        file = [f for f in os.listdir(os.path.join(folder_path, "AAPL")) if os.path.isfile(os.path.join(os.path.join(folder_path, "AAPL"), f))][0]
+        df2 = pd.read_csv(os.path.join(folder_path, "AAPL", file), header = None)
         df2[1] = df2[1].apply(lambda s: float(s[:-1]))
 
-        folder_path_goog= folder_path+"GOOG"
-        files = [f for f in os.listdir(folder_path_goog) if os.path.isfile(os.path.join(folder_path_goog, f))]
-        # Sort the files by creation time
-        files_sorted = sorted(files, key=lambda x: os.path.getctime(os.path.join(folder_path_goog, x)))
-        df3 = pd.DataFrame()
-        for file in files_sorted:
-            path = folder_path+"\\"+file
-            df1 = pd.read_csv(path, header=None)
-            df3 = df3.append(df1)
-
+        file = [f for f in os.listdir(os.path.join(folder_path, "GOOG")) if os.path.isfile(os.path.join(os.path.join(folder_path, "GOOG"), f))][0]
+        df3 = pd.read_csv(os.path.join(folder_path, "GOOG", file), header = None)
         df3[1] = df3[1].apply(lambda s: float(s[:-1]))
 
-        folder_path_nflx= folder_path+"NFLX"
-        files = [f for f in os.listdir(folder_path_nflx) if os.path.isfile(os.path.join(folder_path_nflx, f))]
-        # Sort the files by creation time
-        files_sorted = sorted(files, key=lambda x: os.path.getctime(os.path.join(folder_path_nflx, x)))
-        df4 = pd.DataFrame()
-        for file in files_sorted:
-            path = folder_path+"\\"+file
-            df1 = pd.read_csv(path, header=None)
-            df4 = df4.append(df1)
-        
+        file = [f for f in os.listdir(os.path.join(folder_path, "NFLX")) if os.path.isfile(os.path.join(os.path.join(folder_path, "NFLX"), f))][0]
+        df4 = pd.read_csv(os.path.join(folder_path, "NFLX", file), header = None)
         df4[1] = df4[1].apply(lambda s: float(s[:-1]))
 
-        ys = df.iloc[1:, 1].values
+        ys = df1.iloc[:, 1].values
         xs = list(range(1, len(ys)+1))
         ax1.clear()
         ax1.plot(xs,ys,color='blue')
         ax1.set_title('Tesla price',fontsize=12)
         
-        ys = df2.iloc[1:, 1].values
+        ys = df2.iloc[:, 1].values
         xs = list(range(1, len(ys)+1))
-
         ax2.clear()
         ax2.plot(xs,ys,color='black')
         ax2.set_title('Apple price',fontsize=12)
 
 
         ys = df3.iloc[1:, 1].values
+        xs = list(range(1, len(ys)+1))
         ax3.clear()
         ax3.plot(xs,ys,color='orange')
         ax3.set_title('Google price',fontsize=12)
 
         
         ys = df4.iloc[1:, 1].values
+        xs = list(range(1, len(ys)+1))
         ax4.clear()
         ax4.plot(xs,ys,color='red')
         ax4.set_title('Netflix price',fontsize=12)
